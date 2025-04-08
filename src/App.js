d142667bef379020be0f5347503d8c292e70f961
bang-mini <ybm8308@gmail.com>
2025-04-08 14:06:43 +0900

    front커밋중

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
+
 import './App.css';
 
+import axios from 'axios'
+import { useState } from 'react'
+
 function App() {
+  const [id, setId] = useState();
+  const [pw, setPw] = useState();
+
+
+  function tryLogin(){
+     
+      // axios.get(  
+      //     'http://localhost:8083/controller/login',
+      // // get방식으로 데이터를 보낼때는 params 라는 키값으로 묶어서 보낼것
+      // //http://localhost:8083/controller/login?id=&pw=pw
+
+      //    { params : {id:id, 
+      //     pw:pw}}
+      // )
+      // .then((res)=>{
+      //     console.log(res)
+      //     console.log(res.data)
+
+      //     if(res.data==1){
+      //       // 로그인 성공시 이동페이지
+      //     }
+
+      // })
+
+      axios.post(
+          'http://localhost:8083/controller/login',
+          {id:id, 
+          pw:pw}
+      )
+      .then((res)=>{
+          console.log(res)
+      })
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+ 
+        <h1>Login</h1>
+        <h1>살려줘</h1>
+        <h1>살려줘2</h1>
+         ID : <input onChange={(e)=>setId(e.target.value)}></input>
+         <br></br>
+         PW : <input onChange={(e)=>setPw(e.target.value)}></input>
+         <br></br>
+         <button onClick={tryLogin}>로그인 시도중</button>
+  
     </div>
   );
 }
