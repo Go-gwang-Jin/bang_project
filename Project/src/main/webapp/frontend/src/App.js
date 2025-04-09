2d21aa553d04ad33b51a0dc1e0855b32d6efe340
KGJ <kgwang8116@naver.com>
2025-04-09 12:53:59 +0900

    제발제발 (광진)

--- Project/src/main/webapp/frontend/src/App.js
+++ Project/src/main/webapp/frontend/src/App.js
@@ -1,55 +1,71 @@
-
+import React from 'react';
 import './App.css';
-
 import axios from 'axios'
 import { useState } from 'react'
+import { Routes, Route } from 'react-router-dom';
+import Login from './components/Login';
+import Join from './components/Join';
+import MyCalendar from './components/MyCalendar';
 
 function App() {
-  const [id, setId] = useState();
-  const [pw, setPw] = useState();
+  // const [id, setId] = useState();
+  // const [pw, setPw] = useState();
 
 
-  function tryLogin(){
-     
-      // axios.get(  
-      //     'http://localhost:8083/controller/login',
-      // // get방식으로 데이터를 보낼때는 params 라는 키값으로 묶어서 보낼것
-      // //http://localhost:8083/controller/login?id=&pw=pw
+  // function tryLogin() {
 
-      //    { params : {id:id, 
-      //     pw:pw}}
-      // )
-      // .then((res)=>{
-      //     console.log(res)
-      //     console.log(res.data)
+  //   // axios.get(  
+  //   //     'http://localhost:8083/controller/login',
+  //   // // get방식으로 데이터를 보낼때는 params 라는 키값으로 묶어서 보낼것
+  //   // //http://localhost:8083/controller/login?id=&pw=pw
 
-      //     if(res.data==1){
-      //       // 로그인 성공시 이동페이지
-      //     }
+  //   //    { params : {id:id, 
+  //   //     pw:pw}}
+  //   // )
+  //   // .then((res)=>{
+  //   //     console.log(res)
+  //   //     console.log(res.data)
 
-      // })
+  //   //     if(res.data==1){
+  //   //       // 로그인 성공시 이동페이지
+  //   //     }
 
-      axios.post(
-          'http://localhost:8083/controller/login',
-          {id:id, 
-          pw:pw}
-      )
-      .then((res)=>{
-          console.log(res)
-      })
-  }
+  //   // })
+
+  //   axios.post(
+  //     'http://localhost:8083/controller/login',
+  //     {
+  //       id: id,
+  //       pw: pw
+  //     }
+  //   )
+  //     .then((res) => {
+  //       console.log(res)
+  //     })
+  // }
   return (
-    <div className="App">
- 
+
+    <div>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/join" element={<Join />} />
+        <Route path="/calendar" element={<MyCalendar />} />
+      </Routes>
+
+
+
+      {/* <div className="App">
+
         <h1>Login</h1>
         <h1>살려줘</h1>
         <h1>살려줘2</h1>
-         ID : <input onChange={(e)=>setId(e.target.value)}></input>
-         <br></br>
-         PW : <input onChange={(e)=>setPw(e.target.value)}></input>
-         <br></br>
-         <button onClick={tryLogin}>로그인 시도중</button>
-  
+        ID : <input onChange={(e) => setId(e.target.value)}></input>
+        <br></br>
+        PW : <input onChange={(e) => setPw(e.target.value)}></input>
+        <br></br>
+        <button onClick={tryLogin}>로그인 시도중</button>
+
+      </div> */}
     </div>
   );
 }
